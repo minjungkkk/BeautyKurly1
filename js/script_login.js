bde42f68d8927aa70b7e2def4a51a484fd4209a4
jane <91janekoo@gmail.com>
2026-01-29 17:02:53 +0900

    BeautyKurly

---
+++ js/script_login.js
@@ -0,0 +1,11 @@
+$(function(){
+const togglePassword = document.querySelector('.toggle-password');
+const passwordInput = document.getElementById('password');
+
+togglePassword.addEventListener('click', () => {
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+    togglePassword.classList.toggle('fa-eye-slash');
+});
+
+});
